3e5af2eb7dd96d971514fbea244f35fa6eb22d0f
ShaderKitty <ajossart3@gmail.com>
2021-12-18 22:22:03 -0500

    testing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 #include <geodesuka/core/math.h>
 
+using namespace geodesuka::core;
+
 int main(int argc, char* argv[]) {
 	//ubyte ua = 0x00;
 	//ushort ub = 23;
@@ -12,9 +14,15 @@ int main(int argc, char* argv[]) {
 	//short b = 23;
 	//int c = 69;
 
-	uint4 a(1), b(2);
-	uint4 c(0);
-	c = uint4(0, 0, 1, 0) + uint4(0, 0, 0, 0);
+	math::real3 a(1.0, 0.0, 1.0);
+	float3 b(0.0, 2.0, 3.0);
+
+	float3 c = a + b;
+	
+	//uint4 a(1), b(2);
+	//uint4 c(0);
+	//c = uint4(0, 0, 1, 0) + uint4(0, 0, 0, 0);
+
 	return 0;
 }
 
